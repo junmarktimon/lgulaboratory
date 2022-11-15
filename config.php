2a823d7beb52e9dd743873abf304b504342b7952
junmarktimon <doymakey@gamil.com>
2022-11-15 13:43:04 +0800

    adding lgu files

---
+++ config.php
@@ -0,0 +1,30 @@
+
+
+
+<?php
+	
+	//create variable to hold database configuration 
+	$server_name ="localhost";
+	$db_username = "root";
+	$db_password = "";
+	$db_name = "db_laboratory";
+
+
+	//create database connection	   
+	$connection = mysqli_connect("$server_name","$db_username","$db_password");
+	$dbconfig = mysqli_select_db($connection,$db_name);
+
+
+	//check if database is connected
+	if($dbconfig){
+
+		//if connected no display
+
+	}else{
+
+		//if not connected echo or display the error
+	 	echo "Database Connection Error";
+	 	
+	}
+
+
---
+++ index.php
@@ -0,0 +1,60 @@
+<?php
+
+    include 'header.php';
+
+?>
+
+
+
+
+
+	<div class="text-center mt-5">
+
+		<h1> GINGOOG CITY </h1>
+
+		<img src="logo.jpg" class="rounded-circle">
+		
+	</div>
+
+
+	<div class="container mt-5">
+		<div class="row">
+			<div class="col-4">  </div>
+			<div class="col-4">
+
+				<form>
+				  <div class="form-group">
+				    <label for="exampleInputEmail1"> Username </label>
+				    <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+				  </div>
+				  <div class="form-group">
+				    <label for="exampleInputPassword1">Password</label>
+				    <input type="password" class="form-control" id="exampleInputPassword1">
+				  </div>
+				  <button type="submit" class="btn btn-primary btn-block"> Login </button>
+				</form>
+				
+			</div>
+			<div class="col-4">  </div>
+		</div>
+	</div>
+
+
+
+
+
+	
+
+<?php
+
+	include 'footer.php';
+
+?>
+
+
+
+
+
+
+
+     
